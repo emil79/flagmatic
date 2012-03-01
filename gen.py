fd44cf4950543f104584a7e0cb01cad87fc4da40
Emil Vaughan <emil79@gmail.com>
2012-03-01 01:13:44 +0000

    More improvements to Flagmatic 2 graph generation.

--- gen.py
+++ gen.py
@@ -6,7 +6,9 @@ def degrees(g):
 	edges = g[1]
 	return [len([e for e in edges if x in e]) for x in range(1, n + 1)]
 
-def generate_flags(n, tg):
+def generate_flags(n, tg, forbidden_edge_numbers={}):
+
+	check_forbidden_edge_numbers = len(forbidden_edge_numbers) > 0
 
 	t = tg[0]
 
@@ -21,7 +23,7 @@ def generate_flags(n, tg):
 	
 	new_graphs = [set() for i in range(max_e + 1)]
 	
-	smaller_graphs = generate_flags(n - 1, tg)
+	smaller_graphs = generate_flags(n - 1, tg, forbidden_edge_numbers=forbidden_edge_numbers)
 	
 	possible_nbrs = [p for p in itertools.combinations(range(1, n), 2)]
 
@@ -36,14 +38,19 @@ def generate_flags(n, tg):
 			for nb in itertools.combinations(possible_nbrs, ne):
 			
 				ng = (n, sg[1] + tuple([(v[0], v[1], n) for v in nb]))
+
+				if check_forbidden_edge_numbers:
+					if has_forbidden_edge_numbers(ng, forbidden_edge_numbers):
+						continue
+
 				mng = minimal_isomorph(ng, tg)
 				new_graphs[pe + ne].add(mng)
 
 	return [g for graphs in new_graphs for g in graphs]
 
 
-def generate_graphs(n):
+def generate_graphs(n, forbidden_edge_numbers={}):
 	
 	t = (0, ())
-	return generate_flags(n, t)
+	return generate_flags(n, t, forbidden_edge_numbers=forbidden_edge_numbers)
 	
